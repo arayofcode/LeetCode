dc1ac1d17e1edf91a2cf183d3856c84f43c9b062
Ray <49439106+arayofcode@users.noreply.github.com>
2022-02-26 12:58:02 +0530

    Update two-sum

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,5 +1,16 @@
 class Solution:
+    # Brute-forced solution
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i, val in enumerate(nums):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+    # Efficient solution
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Create a dictionary, store index with the value as key
+        # If target - val exists in dictionary, return indexes
+        # else add val to dictionary
         memo = {}
         for i, val in enumerate(nums):
             if target - val in memo:
