fdee5a1df8be440f12b7a62cd46935ec2d54c023
Ray <49439106+arayofcode@users.noreply.github.com>
2023-11-08 12:38:29 -0800

    class Solution:     def twoSum(self, nums: List[int], target: int) -> List[int]:         memo = {}         for i, val in enumerate(nums):             if target - val in memo:                 return [i, memo[target-val]]             memo[val] = i

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,18 +1,1 @@
-class Solution:
-    # Brute-forced solution
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i, val in enumerate(nums):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-
-    # Efficient solution
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Create a dictionary, store index with the value as key
-        # If target - val exists in dictionary, return indexes
-        # else add val to dictionary
-        memo = {}
-        for i, val in enumerate(nums):
-            if target - val in memo:
-                return [i, memo[target-val]]
-            memo[val] = i
+Time: 76 ms (Beats 42.97%), Space: 17.7 MB (Beats 17.95%)
