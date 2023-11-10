4cdf43589f64cd5635643a9d2c8e9a312ce515a1
Ray <49439106+arayofcode@users.noreply.github.com>
2023-11-10 07:06:10 -0800

    Time: 0 ms (Beats 100.00%), Space: 6.4 MB (Beats 96.51%)

--- 206-reverse-linked-list/206-reverse-linked-list.c
+++ 206-reverse-linked-list/206-reverse-linked-list.c
@@ -10,11 +10,12 @@
 struct ListNode* reverseList(struct ListNode* head){
     struct ListNode* prev = NULL;
     struct ListNode* next = NULL;
-    while(head){
-        next = head -> next;
-        head -> next = prev;
-        prev = head;
-        head = next;
+    struct ListNode* curr = head;
+    while(curr){
+        next = curr -> next;
+        curr -> next = prev;
+        prev = curr;
+        curr = next;
     }
     return prev;
 }
