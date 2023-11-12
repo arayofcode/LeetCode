3665fd51f48b2fdf96672d3248d86b52691c92c6
Ray <49439106+arayofcode@users.noreply.github.com>
2023-11-12 02:52:45 -0800

    Time: 4940 ms (Beats 6.05%), Space: 15 MB (Beats 100.00%)

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,7 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        memo = {}
         for i, val in enumerate(nums):
-            if target - val in memo:
-                return [i, memo[target-val]]
-            memo[val] = i
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
