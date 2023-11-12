eaa664f3097ef8572592f536c15d373314026f6b
Ray <49439106+arayofcode@users.noreply.github.com>
2023-11-12 02:56:32 -0800

    Time: 22 ms (Beats 12.87%), Space: 6.5 MB (Beats 99.52%)

--- 268-missing-number/268-missing-number.c
+++ 268-missing-number/268-missing-number.c
@@ -1,10 +1,24 @@
 
 
 int missingNumber(int* nums, int numsSize){
-    // Sum of all elements from 0 to n
-    int sum = numsSize * (numsSize + 1) / 2;
+    /*
+      * Methods:
+        1. Sort array and find number that isn't present: O(n log n)
+        2. int sum = n(n+1) / 2 then subtract all elements from sum. Final result is the required number: O(n)
+        3. XOR from 0 to n, and all elements of nums: O(n)
+    */
+    
+    // Method 2
+    // int sum = numsSize * (numsSize + 1) / 2;
+    // for(int i = 0; i < numsSize; i++){
+    //     sum -= nums[i];
+    // }
+    // return sum;
+    
+    // Method 3
+    int result = numsSize;
     for(int i = 0; i < numsSize; i++){
-        sum -= nums[i];
+        result ^= i ^ nums[i];
     }
-    return sum;
+    return result;
 }
