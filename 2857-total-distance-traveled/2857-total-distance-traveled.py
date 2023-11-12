463c6ceee017c3fe55a330eaf2b28f896fbebc1c
Ray <49439106+arayofcode@users.noreply.github.com>
2023-11-12 02:58:34 -0800

    Time: 74 ms (Beats 34.48%), Space: 16.3 MB (Beats 46.29%)

--- 2857-total-distance-traveled/2857-total-distance-traveled.py
+++ 2857-total-distance-traveled/2857-total-distance-traveled.py
@@ -1,5 +1,4 @@
 class Solution:
     def distanceTraveled(self, mainTank: int, additionalTank: int) -> int:
-        # return 10*(mainTank + min((mainTank//5), additionalTank))
-        mainTank = mainTank + min((mainTank - 1) // 4, additionalTank)
-        return mainTank * 10
+        return 10 * (mainTank + min((mainTank - 1) // 4, additionalTank))
+        
