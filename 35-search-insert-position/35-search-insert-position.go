4fbc86d7e23a959c144a7060c63c1d789448f5f4
Ray <49439106+arayofcode@users.noreply.github.com>
2023-11-12 02:53:36 -0800

    Time: 4 ms (Beats 77.69%), Space: 3 MB (Beats 96.70%)

---
+++ 35-search-insert-position/35-search-insert-position.go
@@ -0,0 +1,15 @@
+func searchInsert(nums []int, target int) int {
+    low, high := 0, len(nums) - 1
+    mid := low + (high - low) / 2
+    for low <= high {
+        if nums[mid] == target {
+            return mid
+        } else if nums[mid] > target {
+            high = mid - 1
+        } else {
+            low = mid + 1
+        }
+        mid = (low + high) / 2
+    }
+    return low
+}
