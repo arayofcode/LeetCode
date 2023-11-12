24ecde0ab5a0e002563103bbd2af43122b832da8
Ray <49439106+arayofcode@users.noreply.github.com>
2023-11-12 02:58:08 -0800

    Time: 0 ms (Beats 100.00%), Space: 2.4 MB (Beats 84.43%)

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.go
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.go
@@ -1,18 +1,17 @@
 func kidsWithCandies(candies []int, extraCandies int) []bool {
     maxCandies := candies[0]
-    for i := 1; i < len(candies); i++ {
+    length := len(candies)
+    for i := 1; i < length; i++ {
         if candies[i] > maxCandies {
             maxCandies = candies[i]
         }
     }
 
-    var result []bool
-    for i := 0; i < len(candies); i++ {
+    var results = make([]bool, len(candies))
+    for i := 0; i < length; i++ {
         if candies[i] + extraCandies >= maxCandies {
-            result = append(result, true)
-        } else {
-            result = append(result, false)
+            results[i] = true
         }
     }
-    return result
+    return results
 }
