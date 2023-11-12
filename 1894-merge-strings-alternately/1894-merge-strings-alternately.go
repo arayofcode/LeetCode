c78486c4637fd9a2e6649860781680931bb218b1
Ray <49439106+arayofcode@users.noreply.github.com>
2023-11-12 02:58:23 -0800

    Time: 0 ms (Beats 100.00%), Space: 2.5 MB (Beats 30.77%)

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.go
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.go
@@ -1,6 +1,6 @@
 func mergeAlternately(word1 string, word2 string) (merged string) {
     i := 0
-    for i < len(word1) || i < len(word2) {
+    for i < len(word1) && i < len(word2) {
         if i < len(word1) {
             merged += string(word1[i])
         }
@@ -8,6 +8,12 @@ func mergeAlternately(word1 string, word2 string) (merged string) {
             merged += string(word2[i])
         }
         i++
+    }
+    if i < len(word1) {
+        merged += word1[i:]
+    }
+    if i < len(word2) {
+        merged += word2[i:]
     }
     return merged
 }
