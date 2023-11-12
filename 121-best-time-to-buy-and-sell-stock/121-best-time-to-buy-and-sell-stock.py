2790a64217f70b88b200de7c9645f3db789d2733
Ray <49439106+arayofcode@users.noreply.github.com>
2023-11-12 02:54:23 -0800

    Time: 984 ms (Beats 5.02%), Space: 25.3 MB (Beats 99.84%)

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,4 +1,12 @@
-class Solution:    
+class Solution:
+    def maxProfit1(self, prices: List[int]) -> int:
+        maximum = 0
+        for i in range(len(prices)):
+            for j in range(i + 1, len(prices)):
+                if prices[j] - prices[i] > maximum:
+                    maximum = prices[j] - prices[i]
+        return maximum
+    
     def maxProfit(self, prices: List[int]) -> int:
         minimum_so_far = prices[0]
         max_diff = 0
