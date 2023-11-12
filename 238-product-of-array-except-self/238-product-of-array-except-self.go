a05eba720dab5203a131b19d59bfe8216f43b0ee
Ray <49439106+arayofcode@users.noreply.github.com>
2023-11-12 02:56:07 -0800

    Time: 26 ms (Beats 54.71%), Space: 8.5 MB (Beats 10.51%)

--- 238-product-of-array-except-self/238-product-of-array-except-self.go
+++ 238-product-of-array-except-self/238-product-of-array-except-self.go
@@ -12,25 +12,3 @@ func productExceptSelf(nums []int) []int {
     }
     return result
 }
-
-func productExceptSelfArray(nums []int) []int {
-    // Implementation using slices storing prefix and suffix
-    n := len(nums)
-    var prefix = make([]int, n)
-    var suffix = make([]int, n)
-    var result = make([]int, n)
-    
-    for i := 0; i < n; i++ {
-        if i == 0 {
-            prefix[i] = 1
-            suffix[n - 1] = 1
-        } else {
-            prefix[i] = nums[i - 1] * prefix[i - 1]
-            suffix[n - 1 - i] = nums[n - i] * suffix[n - i]
-        }
-    }
-    for i := 0; i < n; i++ {
-        result[i] = prefix[i] * suffix[i]
-    }
-    return result
-}
