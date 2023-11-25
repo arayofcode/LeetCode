f7a6c320f74bd67a4892a2491487ebc120c73c02
Ray <49439106+arayofcode@users.noreply.github.com>
2023-11-25 07:53:52 -0800

    Time: 0 ms (Beats 100.00%), Space: 7.2 MB (Beats 29.28%)

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.go
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.go
@@ -1,24 +1,4 @@
 func reverseWords(s string) (result string) {
-    word := ""
-    for i := 0; i < len(s); i++ {
-        if s[i] == ' ' {
-            if len(word) > 0 {
-                result = word + " " + result
-            }
-            word = ""
-        } else {
-            word += string(s[i])
-        }
-    }
-    if len(word) > 0 {
-        result = word + " " + result
-    }
-    result = result[:len(result) - 1]
-    return
-}
-
-func reverseWordsArray(s string) (result string) {
-    // Split string into words array, join elements with space
     var words []string
     word := ""
     for i := 0; i < len(s); i++ {
