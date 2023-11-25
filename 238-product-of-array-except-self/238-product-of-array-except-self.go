97d48dc5a771709a8d193f691a11b62a770c069c
Ray <49439106+arayofcode@users.noreply.github.com>
2023-11-25 07:55:34 -0800

    Time: 13 ms (Beats 99.38%), Space: 7.2 MB (Beats 77.11%)

--- 238-product-of-array-except-self/238-product-of-array-except-self.go
+++ 238-product-of-array-except-self/238-product-of-array-except-self.go
@@ -1,14 +1,20 @@
 func productExceptSelf(nums []int) []int {
     n := len(nums)
+    var prefix = make([]int, n)
+    var suffix = make([]int, n)
     var result = make([]int, n)
-    left_product, right_product := 1, 1
+    
     for i := 0; i < n; i++ {
-        result[i] = left_product
-        left_product *= nums[i]
+        if i == 0 {
+            prefix[i] = 1
+            suffix[n - 1] = 1
+        } else {
+            prefix[i] = nums[i - 1] * prefix[i - 1]
+            suffix[n - 1 - i] = nums[n - i] * suffix[n - i]
+        }
     }
-    for i := n - 1; i >= 0; i-- {
-        result[i] *= right_product
-        right_product *= nums[i]
+    for i := 0; i < n; i++ {
+        result[i] = prefix[i] * suffix[i]
     }
     return result
 }
