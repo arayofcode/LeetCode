1a3218195db3470da96f4c9f89da93a860d1f5e7
Ray <49439106+arayofcode@users.noreply.github.com>
2023-11-25 07:55:16 -0800

    Time: 17 ms (Beats 95.48%), Space: 7.3 MB (Beats 64.84%)

--- 238-product-of-array-except-self/238-product-of-array-except-self.go
+++ 238-product-of-array-except-self/238-product-of-array-except-self.go
@@ -18,18 +18,3 @@ func productExceptSelf(nums []int) []int {
     }
     return result
 }
-
-func productExceptSelfApproach2(nums []int) []int {
-    n := len(nums)
-    var result = make([]int, n)
-    left_product, right_product := 1, 1
-    for i := 0; i < n; i++ {
-        result[i] = left_product
-        left_product *= nums[i]
-    }
-    for i := n - 1; i >= 0; i-- {
-        result[i] *= right_product
-        right_product *= nums[i]
-    }
-    return result
-}
