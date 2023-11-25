99554f56818e62d8a889055227310de4849c869e
Ray <49439106+arayofcode@users.noreply.github.com>
2023-11-25 07:56:14 -0800

    Time: 104 ms (Beats 89.86%), Space: 18.9 MB (Beats 85.36%)

---
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.go
@@ -0,0 +1,16 @@
+func increasingTriplet(nums []int) bool {
+    minimum := math.MaxInt
+    secondMinimum := math.MaxInt
+    n := len(nums)
+    for i := 0; i < n; i++ {
+        num := nums[i]
+        if num <= minimum {
+            minimum = num
+        } else if num <= secondMinimum {
+            secondMinimum = num
+        } else {
+            return true
+        }
+    }
+    return false
+}
