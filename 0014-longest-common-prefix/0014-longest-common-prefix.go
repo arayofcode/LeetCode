b24267d5f6da86b0e003d9cb1211584a84556528
Ray <49439106+arayofcode@users.noreply.github.com>
2025-01-04 06:06:50 +0530

    Time: 0 ms (100%), Space: 4.3 MB (52.35%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.go
+++ 0014-longest-common-prefix/0014-longest-common-prefix.go
@@ -1,5 +1,42 @@
+// Binary Search Solution: O(n log k), k = len(shortest string)
+// Find smallest word size
+// From size / 2, check if all substrings of this size have common prefix
+// If yes, check between size / 2 + 1, high
+// Else check low to size / 2 - 1
 func longestCommonPrefix(strs []string) string {
-	prefix := ""
+	if len(strs) == 1 {
+        return strs[0]
+    }
+
+    minSize := smallestStringSize(strs)
+	low := 0
+	high := minSize
+	for low <= high {
+		mid := (low + high) / 2
+        if hasCommonPrefix(strs, mid) {
+            low = mid + 1
+        } else {
+            high = mid - 1
+        }
+	}
+    return strs[0][:(low + high)/2]
+}
+
+func hasCommonPrefix(strs []string, mid int) bool {
+    prefix := strs[0][:mid]
+    for _, currStr := range strs {
+        if prefix != currStr[:mid] {
+            return false
+        }
+    }
+    return true
+}
+
+// Brute Force: O(m*n)
+// For each index in a string i, check if string[i]
+// is same for all strings in array
+func longestCommonPrefixBruteForce(strs []string) string {
+    prefix := ""
 	for i := 0; i < smallestStringSize(strs); i++ {
 		curr := strs[0][i]
 		for _, currStr := range strs {
