4672affef60549289a0763c4a22c652eca1ed8ba
Ray <49439106+arayofcode@users.noreply.github.com>
2025-01-07 06:20:38 +0530

    Time: 0 ms (100%), Space: 4.2 MB (1.46%) - LeetHub

--- 0027-remove-element/0027-remove-element.go
+++ 0027-remove-element/0027-remove-element.go
@@ -1,8 +1,12 @@
+func removeElement(nums []int, val int) int {
+    return twoPointers(nums, val)
+}
+
 // Approach 1 - Two Pointers
 // One pointer to iterate, the other to swap
 // Start from 0. If non-val, swap, and increase count
 // Time: O(n) Space: O(1)
-func removeElement(nums []int, val int) int {
+func twoPointers(nums []int, val int) int {
     k := 0
     for _, num := range nums {
         if num != val {
@@ -11,4 +15,41 @@ func removeElement(nums []int, val int) int {
         }
     }
     return k
+}
+
+// Approach 2 - Hash Table:
+// Create a frequency table for all elements in nums
+// From table, if key is not val, insert it table[key]
+// times in nums. Return count of such elements
+// Time: O(n), Space: O(n)
+func hashTable(nums []int, val int) (count int) {
+    table := make(map[int]int)
+    for _, num := range nums {
+        table[num]++
+    }
+
+    for element, frequency := range table {
+        if element != val {
+            for i := 0; i < frequency; i++ {
+                nums[count] = element
+                count++
+            }
+        }
+    }
+    return count
+}
+
+// Approach 3 - Slice off any val found
+// Create new slice, append non-val elements
+// Copy this slice back to nums
+// Time: O(n) Space: O(n)
+func slicing(nums []int, val int) int {
+    var result []int
+    for _, num := range nums {
+        if num != val {
+            result = append(result, num)
+        }
+    }
+    copy(nums, result)
+    return len(result)
 }
