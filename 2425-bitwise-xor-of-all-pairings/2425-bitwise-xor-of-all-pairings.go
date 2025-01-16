b38883ea761931ef4bacc392d0e8a3cb0f287586
Ray <49439106+arayofcode@users.noreply.github.com>
2025-01-17 00:14:56 +0530

    Time: 0 ms (100%), Space: 12.6 MB (20.83%) - LeetHub

--- 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.go
+++ 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.go
@@ -1,31 +1,13 @@
-func xorAllNums(nums1 []int, nums2 []int) int {
-    m, n := len(nums1), len(nums2)
-    evenM, evenN := m % 2 == 0, n % 2 == 0
-    if evenM && evenN {
-        return 0
-    } else if evenM {
-        result := 0
+func xorAllNums(nums1 []int, nums2 []int) (result int) {
+    if len(nums2) % 2 != 0 {
         for _, num1 := range nums1 {
             result ^= num1
         }
-        return result
-    } else if evenN {
-        result := 0
+    }
+    if len(nums1) % 2 != 0 {
         for _, num2 := range nums2 {
             result ^= num2
         }
-        return result
     }
-    i, j, result := 0, 0, 0
-    for i < m || j < n {
-        if i < m {
-            result ^= nums1[i]
-            i++
-        }
-        if j < n {
-            result ^= nums2[j]
-            j++
-        }
-    }
-    return result
+    return
 }
