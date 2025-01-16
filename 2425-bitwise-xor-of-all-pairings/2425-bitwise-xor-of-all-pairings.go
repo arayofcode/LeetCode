a0848e711f0aff6fd8afcffda633e44ff2251369
Ray <49439106+arayofcode@users.noreply.github.com>
2025-01-17 00:16:07 +0530

    Time: 0 ms (100%), Space: 11.4 MB (91.67%) - LeetHub

--- 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.go
+++ 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.go
@@ -1,4 +1,5 @@
-func xorAllNums(nums1 []int, nums2 []int) (result int) {
+func xorAllNums(nums1 []int, nums2 []int) int {
+    result := 0
     if len(nums2) % 2 != 0 {
         for _, num1 := range nums1 {
             result ^= num1
@@ -9,5 +10,5 @@ func xorAllNums(nums1 []int, nums2 []int) (result int) {
             result ^= num2
         }
     }
-    return
+    return result
 }
