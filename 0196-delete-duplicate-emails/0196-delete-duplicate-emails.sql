204f500909565bcd9427befa48e81ef496b2839d
Ray <49439106+arayofcode@users.noreply.github.com>
2025-04-07 10:37:17 +0530

    Time: 387 ms (52.9%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,4 +1,13 @@
+WITH cte AS (
+  SELECT id, ROW_NUMBER() OVER (PARTITION BY email ORDER BY id) AS row_num
+  FROM Person
+)
 DELETE FROM Person
-WHERE id NOT IN (
-    SELECT MIN(id) FROM Person GROUP BY email
-);
+WHERE id IN (SELECT id FROM cte WHERE row_num > 1);
+-- DELETE FROM Person p1 USING Person p2
+-- WHERE p1.email = p2.email AND p1.id > p2.id;
+
+-- DELETE FROM Person
+-- WHERE id NOT IN (
+--     SELECT MIN(id) FROM Person GROUP BY email
+-- );
