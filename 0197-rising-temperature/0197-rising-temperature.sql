d60b9150120444ded4f3c53e172166729ced6272
Ray <49439106+arayofcode@users.noreply.github.com>
2025-04-11 14:09:52 +0530

    Time: 1799 ms (5.02%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,4 +1,4 @@
-SELECT w1.id
-FROM Weather w1 JOIN Weather w2
-    ON w2.recordDate = w1.recordDate - interval '1 day'
-WHERE w1.temperature > w2.temperature;
+SELECT w2.id "Id"
+FROM Weather w2 LEFT JOIN Weather w1
+    ON w2.recordDate = w1.recordDate + INTERVAL '1 day'
+WHERE w2.temperature > w1.temperature
