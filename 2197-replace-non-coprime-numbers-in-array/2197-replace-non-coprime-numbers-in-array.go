0105a564432f858f35e4f718b029023769f6261f
Ray <49439106+arayofcode@users.noreply.github.com>
2025-09-16 22:05:00 +0530

    Time: 5 ms (100%), Space: 10.2 MB (100%) - LeetHub

--- 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.go
+++ 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.go
@@ -1,38 +1,24 @@
 func replaceNonCoprimes(nums []int) []int {
-    results := make([]int, 0, len(nums))
-    results = append(results, nums[0])
+    stackSize := 1 // assuming nums[0] is in position
+
     for i := 1; i < len(nums); i++ {
         current := nums[i]
-        for len(results) > 0 {
-            gcd := GCD(results[len(results) - 1], current)
+
+        for stackSize > 0 {
+            gcd := GCD(nums[stackSize - 1], current)
             if gcd > 1 {
-                current = LCM(results[len(results) - 1], current, gcd)
-                results = results[:len(results) - 1]
+                current = LCM(nums[stackSize - 1], current, gcd)
+                stackSize--
             } else {
                 break
             }
         }
-        results = append(results, current)
-    }
-    return results
-}
 
-// Gives TLE
-// func replaceNonCoprimes(nums []int) []int {
-//     for i := 0; i < len(nums) - 1; {
-//         gcd := GCD(nums[i], nums[i+1])
-//         if gcd > 1 {
-//             nums[i] = LCM(nums[i], nums[i+1], gcd)
-//             nums = slices.Delete(nums, i+1, i+2)
-//             if i > 0 {
-//                 i--
-//             }
-//             continue
-//         }
-//         i++
-//     }
-//     return nums
-// }
+        nums[stackSize] = current
+        stackSize++
+    }
+    return nums[:stackSize]
+}
 
 func LCM(a int, b int, gcd int) int {
     return a * b / gcd
