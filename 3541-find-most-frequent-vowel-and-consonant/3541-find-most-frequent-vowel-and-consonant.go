9a99cf5b8f87ecc8fd556e340ec8d59a755d8f64
Ray <49439106+arayofcode@users.noreply.github.com>
2025-09-20 03:58:58 +0530

    Time: 0 ms (100%), Space: 4.2 MB (39.31%) - LeetHub

--- 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.go
+++ 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.go
@@ -1,42 +1,20 @@
 func maxFreqSum(s string) int {
-    maxVowelFreq := 0
-    vowelFreq := [5]int{}
-    maxConsonantFreq := 0
-    consonantFreq := make(map[byte]int, 21)
-    for i := 0; i < len(s); i++ {
-        chr := s[i]
-        switch chr {
-        case 'a':
-            vowelFreq[0]++
-            if vowelFreq[0] > maxVowelFreq {
-                maxVowelFreq = vowelFreq[0]
+    letters := [26]int{}
+    vowel := 0
+    consonant := 0
+    for _, chr := range s {
+        i := int(chr - 'a')
+        letters[i]++
+
+        if i == 0 || i == 4 || i == 8 || i == 14 || i == 20 {
+            if letters[i] > vowel {
+                vowel = letters[i]
             }
-        case 'e':
-            vowelFreq[1]++
-            if vowelFreq[1] > maxVowelFreq {
-                maxVowelFreq = vowelFreq[1]
-            }
-        case 'i':
-            vowelFreq[2]++
-            if vowelFreq[2] > maxVowelFreq {
-                maxVowelFreq = vowelFreq[2]
-            }
-        case 'o':
-            vowelFreq[3]++
-            if vowelFreq[3] > maxVowelFreq {
-                maxVowelFreq = vowelFreq[3]
-            }
-        case 'u':
-            vowelFreq[4]++
-            if vowelFreq[4] > maxVowelFreq {
-                maxVowelFreq = vowelFreq[4]
-            }
-        default:
-            consonantFreq[chr]++
-            if consonantFreq[chr] > maxConsonantFreq {
-                maxConsonantFreq = consonantFreq[chr]
+        } else {
+            if letters[i] > consonant {
+                consonant = letters[i]
             }
         }
     }
-    return maxVowelFreq + maxConsonantFreq
+    return vowel + consonant
 }
