16de412901299868afd13bf07c5ff060d326de9f
Ray <49439106+arayofcode@users.noreply.github.com>
2025-09-25 23:08:41 +0530

    Time: 0 ms (100%), Space: 4.9 MB (57.21%) - LeetHub

--- 0120-triangle/0120-triangle.go
+++ 0120-triangle/0120-triangle.go
@@ -1,13 +1,22 @@
+func minimumTotal(triangle [][]int) int {
+    for i := len(triangle) - 2; i >= 0; i-- {
+        for j := 0; j <= i; j++ {
+            if triangle[i+1][j] < triangle[i+1][j+1] {
+                triangle[i][j] += triangle[i+1][j]
+            } else {
+                triangle[i][j] += triangle[i+1][j+1]
+            }
+        }
+    }
+    return triangle[0][0]
+}
+
 type mapKey struct {
     a int
     b int
 }
 
-func minimumTotal(triangle [][]int) int {
-    return getMinSum(triangle, 0, 0, make(map[mapKey]int))
-}
-
-func getMinSum(triangle [][]int, i int, j int, memo map[mapKey]int) int {
+func sumTillNApproach(triangle [][]int, i int, j int, memo map[mapKey]int) int {
     if i >= len(triangle) || j >= len(triangle[i]) {
         return 0
     }
@@ -17,9 +26,17 @@ func getMinSum(triangle [][]int, i int, j int, memo map[mapKey]int) int {
     }
 
     result := triangle[i][j] + min(
-        getMinSum(triangle, i+1, j, memo),
-        getMinSum(triangle, i+1, j+1, memo),
+        sumTillNApproach(triangle, i+1, j, memo),
+        sumTillNApproach(triangle, i+1, j+1, memo),
     )
     memo[key] = result
     return result
+}
+
+func findSumApproach(triangle [][]int, currSum int, i int, j int) int {
+    if i >= len(triangle) || j >= len(triangle[i]) {
+        return currSum
+    }
+    currSum += triangle[i][j]
+    return min(findSumApproach(triangle, currSum, i + 1, j), findSumApproach(triangle, currSum, i + 1, j + 1))
 }
