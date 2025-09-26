51a5d3234bfac7e4402ce3c9b721218fa37bac49
Ray <49439106+arayofcode@users.noreply.github.com>
2025-09-26 12:39:17 +0530

    Time: 21 ms (53.92%), Space: 4.8 MB (99.02%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.go
+++ 0611-valid-triangle-number/0611-valid-triangle-number.go
@@ -1,15 +1,27 @@
 func triangleNumber(nums []int) int {
-    return bruteForceApproach(nums)
+    return twoPointersApproach(nums)
 }
 
-// func sortedApproach(nums []int) int {
-//     sort.Ints(nums)
-//     for i := 0; i < len(nums) - 2; i++ {
-//         if nums[i] + nums[i+1] <= nums[i+2] {
+// O(n log n + n^2) = O(n^2)
+func twoPointersApproach(nums []int) int {
+    sort.Ints(nums)
+    count := 0
 
-//         }
-//     }
-// }
+    // when k is the largest, find count of all possible triplets
+    for k := 2; k < len(nums); k++ {
+        i, j := 0, k-1
+        for i < j {
+            if nums[i] + nums[j] > nums[k] {
+                count += j - i
+                j--
+            } else {
+                i++
+            }
+        }
+    }
+    
+    return count
+}
 
 // O(n^3)
 func bruteForceApproach(nums []int) int {
