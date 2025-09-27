40995270437a6fb02772aafa774acadfc84aba69
Ray <49439106+arayofcode@users.noreply.github.com>
2025-09-28 02:34:11 +0530

    Time: 27 ms (42.31%), Space: 9.3 MB (65.38%) - LeetHub

---
+++ 1797-design-authentication-manager/1797-design-authentication-manager.go
@@ -0,0 +1,70 @@
+type countNode struct {
+    count  int
+    // index from expiries
+    index  int
+}
+
+type AuthenticationManager struct {
+    ttl      int
+    // token ID to expiry
+    tokens   map[string]int
+    // expiry time to count
+    count    map[int]*countNode
+    // maintaining all expiries
+    expiries []int
+}
+
+func Constructor(timeToLive int) AuthenticationManager {
+    return AuthenticationManager{
+        ttl : timeToLive,
+        tokens: make(map[string]int),
+        count: make(map[int]*countNode),
+        expiries: []int{},
+    }
+}
+
+func (this *AuthenticationManager) Generate(tokenId string, currentTime int)  {
+    expiry := currentTime + this.ttl
+    this.tokens[tokenId] = expiry
+    if _, found := this.count[expiry]; !found {
+        // since generate will have strictly increasing currentTime
+        // this is a sorted array
+        this.expiries = append(this.expiries, expiry)
+        this.count[expiry] = &countNode{
+            count: 1,
+            index: len(this.expiries) - 1,
+        }
+    } else {
+        this.count[expiry].count++
+    }
+}
+
+func (this *AuthenticationManager) Renew(tokenId string, currentTime int)  {
+    if expiry, found := this.tokens[tokenId]; found && expiry > currentTime {
+        // Remove its count from existing expiry, and add it to new expiry
+        if this.count[expiry].count > 0 {
+            this.count[expiry].count--
+        }
+        newExpiry := currentTime + this.ttl
+        this.tokens[tokenId] = newExpiry
+        if _, found := this.count[newExpiry]; !found {
+            this.expiries = append(this.expiries, newExpiry)
+            this.count[newExpiry] = &countNode{
+                count: 1,
+                index: len(this.expiries) - 1,
+            }
+        } else {
+            this.count[newExpiry].count++
+        }
+    }
+}
+
+func (this *AuthenticationManager) CountUnexpiredTokens(currentTime int) int {
+    idx, _ := slices.BinarySearch(this.expiries, currentTime + 1)
+
+    count := 0
+    for _, expiry := range this.expiries[idx:] {
+        count += this.count[expiry].count
+    }
+    return count
+}
