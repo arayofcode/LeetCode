c74781928859aba15abc0f515eb253d4f872c101
Ray <49439106+arayofcode@users.noreply.github.com>
2025-10-08 03:33:11 +0530

    Time: 18 ms (67.82%), Space: 33.7 MB (83.91%) - LeetHub

---
+++ 2043-simple-bank-system/2043-simple-bank-system.go
@@ -0,0 +1,47 @@
+type Bank struct {
+    total    int
+    balances []int64
+}
+
+
+func Constructor(balance []int64) Bank {
+    return Bank{
+        total: len(balance),
+        balances: append([]int64{0}, balance...),
+    }
+}
+
+
+func (this *Bank) Transfer(account1 int, account2 int, money int64) bool {
+    if this.validAccount(account1) && this.validAccount(account2) && this.balances[account1] >= money {
+        this.balances[account1] -= money
+        this.balances[account2] += money
+        return true
+    }
+    return false
+}
+
+
+func (this *Bank) Deposit(account int, money int64) bool {
+    if this.validAccount(account) {
+        this.balances[account] += money
+        return true
+    }
+    return false
+}
+
+
+func (this *Bank) Withdraw(account int, money int64) bool {
+    if this.validAccount(account) && money <= this.balances[account] {
+        this.balances[account] -= money
+        return true
+    }
+    return false
+}
+
+func (this *Bank) validAccount(account int) bool {
+    if account >= 1 && account <= this.total {
+        return true
+    }
+    return false
+}
