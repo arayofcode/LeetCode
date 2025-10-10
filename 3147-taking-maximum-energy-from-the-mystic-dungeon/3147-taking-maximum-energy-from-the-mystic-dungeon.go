a920be44d22c4b1b491a0513a16801c2aa75e645
Ray <49439106+arayofcode@users.noreply.github.com>
2025-10-10 16:20:42 +0530

    Time: 141 ms (100%), Space: 12.5 MB (0%) - LeetHub

---
+++ 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.go
@@ -0,0 +1,14 @@
+func maximumEnergy(energy []int, k int) int {
+    n := len(energy)
+    for i := n-1-k; i >= 0; i-- {
+        energy[i] += energy[i+k]
+    }
+
+    max := -9999999
+    for i := n-1; i >= 0; i-- {
+        if energy[i] > max {
+            max = energy[i]
+        }
+    }
+    return max
+}
